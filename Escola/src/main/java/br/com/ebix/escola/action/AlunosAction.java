e03c4a733c994a2a8a8070cf796ba73f0c585ebf
lucas.rosa <lucas.rosa@lucasrosa.wdev.int>
2021-09-29 17:23:57 -0300

    Definindo os métodos.

--- Escola/src/main/java/br/com/ebix/escola/action/AlunosAction.java
+++ Escola/src/main/java/br/com/ebix/escola/action/AlunosAction.java
@@ -1,15 +1,40 @@
 package br.com.ebix.escola.action;
 
-import com.opensymphony.xwork2.ActionSupport;
+import java.util.ArrayList;
+import java.util.List;
 
-public class AlunosAction extends ActionSupport {
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import br.com.ebix.escola.model.Aluno;
+
+public class AlunosAction extends ActionSupport implements ModelDriven<Object> {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-	public String execute() {
+	
+	private Aluno aluno = new Aluno();
+	private List<Aluno> alunos = new ArrayList<Aluno>();
+	
+	public String listar() {
+		// alunos = alunoDao.obterAlunos();
 		return "success";
+	}
+	
+	public String adicionar() {
+		// alunoDao.adicionar(aluno);
+		return "success";
+	}
+	
+	public String remover() {
+		// alunoDao.remover(aluno);
+		return "success";
+	}
+	
+	@Override
+	public Object getModel() {
+		return alunos;
 	}
 }
--- Escola/src/main/java/br/com/ebix/escola/action/MateriasAction.java
+++ Escola/src/main/java/br/com/ebix/escola/action/MateriasAction.java
@@ -1,15 +1,40 @@
 package br.com.ebix.escola.action;
 
-import com.opensymphony.xwork2.ActionSupport;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MateriasAction extends ActionSupport {
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import br.com.ebix.escola.model.Materia;
+
+public class MateriasAction extends ActionSupport implements ModelDriven<Object> {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public String execute() {
+	private Materia materia = new Materia();
+	private List<Materia> materias = new ArrayList<Materia>();
+	
+	public String listar() {
+		// materias = materiasDao.obterMaterias();
 		return "success";
+	}
+
+	public String adicionar() {
+		// materiasDao.adicionar(materia);
+		return "success";
+	}
+
+	public String remover() {
+		// materiaDao.remover(materia);
+		return "success";
+	}
+	
+	@Override
+	public Object getModel() {
+		return materias;
 	}
 }
