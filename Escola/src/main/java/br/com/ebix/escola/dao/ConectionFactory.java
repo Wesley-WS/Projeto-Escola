3a7a6b8b82ba558a9d1b421874908737a0a86a83
lucas.rosa <lucas.rosa@lucasrosa.wdev.int>
2021-09-30 14:11:27 -0300

    Porta do mysql alterada

--- Escola/src/main/java/br/com/ebix/escola/dao/ConectionFactory.java
+++ Escola/src/main/java/br/com/ebix/escola/dao/ConectionFactory.java
@@ -10,10 +10,9 @@ public class ConectionFactory {
 			try {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}// Faltando alterar as portas do banco
-			return DriverManager.getConnection("jdbc:mysql://localhost:3307/escola?useSSL=false", "root", "uut3E26MPmxRQNd4");
+			}
+			return DriverManager.getConnection("jdbc:mysql://localhost:3310/escola?useSSL=false", "root", "uut3E26MPmxRQNd4");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
