ffa1ab94ed03cc24a82b2be89867708081db8e90
lucas.rosa <lucas.rosa@lucasrosa.wdev.int>
2021-09-30 16:42:09 -0300

    Implementando a interface façade de materias

---
+++ Escola/src/main/java/br/com/ebix/escola/facade/MateriaFacade.java
@@ -0,0 +1,8 @@
+package br.com.ebix.escola.facade;
+
+import br.com.ebix.escola.model.Crud;
+import br.com.ebix.escola.model.Materia;
+
+public interface MateriaFacade extends Crud<Materia> {
+
+}
---
+++ Escola/src/main/java/br/com/ebix/escola/facade/MateriaFacadeImpl.java
@@ -0,0 +1,40 @@
+package br.com.ebix.escola.facade;
+
+import java.util.List;
+import java.util.Optional;
+
+import br.com.ebix.escola.model.Materia;
+
+public class MateriaFacadeImpl implements MateriaFacade {
+
+	@Override
+	public Optional<Materia> get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Materia> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(Materia t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Materia t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Materia t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
