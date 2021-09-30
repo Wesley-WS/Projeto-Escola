1ad5a1ed67d8453d6bac8ec6e082c457b545e07f
filipe.rosa <filipe.rosa@filiperosa.wdev.int>
2021-09-30 16:48:38 -0300

    Adicionei os atributos e os métodos do modelo Materia

--- Escola/src/main/java/br/com/ebix/escola/model/Materia.java
+++ Escola/src/main/java/br/com/ebix/escola/model/Materia.java
@@ -1,5 +1,42 @@
 package br.com.ebix.escola.model;
 
 public class Materia {
+	private long cod_materia;
+	private String nome;
+	private String sigla;
+	
+	public Materia() {
+		
+	}
 
+	public Materia(String nome, String sigla) {
+		super();
+		this.nome = nome;
+		this.sigla = sigla;
+	}
+
+	public long getCod_materia() {
+		return cod_materia;
+	}
+
+	public void setCod_materia(long cod_materia) {
+		this.cod_materia = cod_materia;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	
 }
