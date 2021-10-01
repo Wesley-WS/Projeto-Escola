a1911741f9590821485d92cd2a78f91fdcd7c6ee
wesley.santos <wesley.santos@wesleysantos.wdev.int>
2021-10-01 16:24:33 -0300

    ajuste

--- Escola/src/main/java/br/com/ebix/escola/action/ProfessorAction.java
+++ Escola/src/main/java/br/com/ebix/escola/action/ProfessorAction.java
@@ -5,37 +5,32 @@ import java.util.List;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
-import br.com.ebix.escola.dao.AlunoDaoImpl;
-import br.com.ebix.escola.facade.AlunoFacadeImpl;
-import br.com.ebix.escola.model.Aluno;
-
-public class AlunosAction extends ActionSupport /*implements ModelDriven<Object>*/ {
+public class ProfessorAction extends ActionSupport /*implements ModelDriven<Object>*/ {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private AlunoFacadeImpl alunoFacadeImpl = new AlunoFacadeImpl();
+	private ProfessorFacadeImpl professorFacadeImpl = new ProfessorFacadeImpl();
 	
-	// private Aluno aluno = new Aluno();
 	
-	private List<Aluno> alunos;
+	
+	private List<Professor> professores;
 
 	//usando esse metodo a pagina jsp consegue pegar a lista da action
-	public List<Aluno> getAlunos() {
-		return alunos;
+	public List<Professor> getProfessores() {
+		return Professores;
 	}
 
 	public String listar() {
-		alunos = alunoFacadeImpl.getAll();
+		professores = professorFacadeImpl.getAll();
 		return "success";
 	}
 	
 	public String adicionar() {
-		Aluno aluno = new Aluno();
-		
-		if(alunoFacadeImpl.add(aluno)) {
+		Professor professor = new Professor();
+		if(professorFacadeImpl.add(professor)) {
 			return "success";
 		} else {
 			return "error";
