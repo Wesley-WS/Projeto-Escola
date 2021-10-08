a4fa4bc49777580fc239a466f0981b8680c1168b
lucas.rosa <lucas.rosa@lucasrosa.wdev.int>
2021-10-08 17:13:37 -0300

    Criando método para converter String para Calendar

--- Escola/src/main/java/br/com/ebix/escola/utils/ConverteDataUtil.java
+++ Escola/src/main/java/br/com/ebix/escola/utils/ConverteDataUtil.java
@@ -1,5 +1,7 @@
 package br.com.ebix.escola.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class ConverteDataUtil {
@@ -7,6 +9,18 @@ public class ConverteDataUtil {
 		Calendar calendario = Calendar.getInstance();
 		calendario.setTime(date);
 		return calendario;
+	}
+	
+	public static Calendar converterStringParaCalendar(String dataStr) {
+		try {
+			java.util.Date date = new SimpleDateFormat("yyyy-MM-dd").parse(dataStr);
+			Calendar dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(date);
+			return dataNascimento;
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public static java.sql.Date converterCalendarParaDatesql(Calendar calendar) {
