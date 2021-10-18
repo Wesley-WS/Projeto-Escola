27daac6a5c1d7eed72654dc4c5ed3c48cd3ed6aa
wesley.santos <wesley.santos@wesleysantos.wdev.int>
2021-10-18 15:30:42 -0300

    ajustando mascara de cpf

--- Escola/src/main/webapp/assets/js/app.js
+++ Escola/src/main/webapp/assets/js/app.js
@@ -90,7 +90,7 @@ function obterMensagemMateria(){
 function carregarMascaras() {
 	let { cpf, telCelular, telResidencial, dataNascimento } = obterCamposPessoa();
 		
-	cpf.mask('000.000.000-00', {reverse: true});
+	cpf.mask('000.000.000-00', {reverse: false});
 	telCelular.mask('(00)00000-0000', {reverse: false});
 	telResidencial.mask('(00)0000-0000', {reverse: false});
 	dataNascimento.mask('00/00/0000', {reverse: true});
